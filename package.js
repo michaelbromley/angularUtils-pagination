ff5ed4833330295eac6067f3f688ad9bcd7265a6
Michael Bromley <michael@michaelbromley.co.uk>
2015-10-01 13:03:44 +0200

    Update to version 0.9.0
    See release notes

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'angularutils:pagination',
   summary: 'Magical automatic pagination for anything in AngularJS',
-  version: '0.8.4',
+  version: '0.9.0',
   git: 'https://github.com/Urigo/angularUtils-pagination'
 });
 
