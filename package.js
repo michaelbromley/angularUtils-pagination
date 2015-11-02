b61064f6123f855648b2444b9f3102a845dd6943
Michael Bromley <michael@michaelbromley.co.uk>
2015-11-02 11:05:01 +0100

    Update to 0.9.2
    (bug fix) See release notes.

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
   name: 'angularutils:pagination',
   summary: 'Magical automatic pagination for anything in AngularJS',
-  version: '0.9.1',
-  git: 'https://github.com/Urigo/angularUtils-pagination'
+  version: '0.9.2',
+  git: 'https://github.com/michaelbromley/angularUtils-pagination'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom(['METEOR@0.9.0', 'METEOR@1.0']);
 
-  api.use('angular:angular@1.2.24', 'client');
+  api.use('angular:angular@1.4.0', 'client');
 
   api.addFiles('dirPagination.js', 'client');
 });
